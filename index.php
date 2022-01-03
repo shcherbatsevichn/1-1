8175b87fbde52333c91e36a0ab1dbb7ae755a787
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-01-03 14:41:42 +0300

    1-1-1-1-1

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 //Определить  количество  цифр,  меньших  5,  используемых  при  
 //записи натурального числа N.
 
-$n = 189533;  
+$n = 16657114;  
 $value = $n;
 
 $num = 0;
 $counter = 0;
-do{
+while($value != 0){
     $num = ($value / 10 - floor($value / 10) )*10;
     $value = floor($value / 10);
     if($num < 5){
         $counter++;
     }    
-}while($value != 0);
+}
 
 echo ("Количество цифр < 5 -".$counter);
